58ac451d8529887b63eaeb2494c676d1b6e02077
Geomit <georgemitropoulos92@gmail.com>
2018-10-29 13:40:21 +0200

    New Project - My Calculator

--- MyCalculator/Program.cs
+++ MyCalculator/Program.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace MyCalculator
 {
@@ -10,12 +7,7 @@ namespace MyCalculator
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            
         }
     }
 }
